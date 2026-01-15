e6e36487646f090e43ade0088e7fd781c00662d0
ayoub elfaqir <ayoub.elfaqir1@gmail.com>
2026-01-15 10:22:21 +0100

    base controller setup

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,17 +1,15 @@
 <?php
 namespace App\core;
 
-class Controller
+abstract class Controller
 {
     protected $view;
     
-    public function __construct()
-    {
+    public function __construct() {
         $this->view = new View();
     }
-    
-    protected function render($template, $data = [])
-    {
+
+    public function render($template , $data = [] ) {
         return $this->view->render($template, $data);
     }
 }
