81030f4c347301a116428c6ebfa5ca029a1a67c4
ayoub elfaqir <ayoub.elfaqir1@gmail.com>
2026-01-16 19:59:24 +0100

    Add core MVC components: Router, Controller, and View

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,12 +1,12 @@
 <?php
 namespace App\core;
-
 abstract class Controller
 {
     protected $view;
-    
+    protected $auth;
     public function __construct() {
         $this->view = new View();
+        $this->auth = new Auth();
     }
 
     public function render($template , $data = [] ) {
